5c4aec90349a739a22075c65ddaea6b81ae8a908
VervEnergy <VervEnergy@protonmail.com>
2018-03-14 18:21:59 +0000

    Clearing out old files from master branch.

--- migrations/1_initial_migration.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const util = require('../helper/util');
-const Migrations = artifacts.require('./Migrations.sol');
-
-module.exports = deployer => {
-  deployer.deploy(Migrations, { gas: util.gas.amount.Migrations });
-};
