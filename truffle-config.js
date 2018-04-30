3fe1914f9abdb294932ee10fbe45507c3857bc43
VervEnergy <VervEnergy@protonmail.com>
2018-04-30 23:01:23 +0100

    Added the new readme n settings

---
+++ truffle-config.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const util = require('./helper/util');
+
+module.exports = {
+  networks: {
+    test: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      gas: util.gas.maxGas,
+      gasPrice: util.gas.price,
+    },
+    testnet: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 3,
+      gas: util.gas.maxGas,
+    },
+    mainnet: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 1,
+      gas: util.gas.maxGas,
+      gasPrice: util.gas.price,
+    },
+  },
+  solc: { optimizer: { enabled: true, runs: 200 } },
+};
