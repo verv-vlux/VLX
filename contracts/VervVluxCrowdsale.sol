fa92eda69ed6c69a21af1932fd0649ecaee022a3
MulanSoldier <MulanSoldier@protonmail.com>
2018-07-16 11:12:45 +0100

    Updated marcet cap comment to link to whitepaper.

--- contracts/VervVluxCrowdsale.sol
+++ contracts/VervVluxCrowdsale.sol
@@ -100,7 +100,7 @@ contract VervVluxCrowdsale is CappedCrowdsale, Ownable, Pausable {
     // _owner = (TBD)
     // _companyWallet = (TBD)
     // _wallet = (TBD)
-    // _cap = amount of wei equivalent to £25M
+    // _cap = to be defined in compliance with the whitepaper
     function VervVluxCrowdsale(
         address _owner,
         address _companyWallet,
@@ -236,7 +236,7 @@ contract VervVluxCrowdsale is CappedCrowdsale, Ownable, Pausable {
         endTime = newEndTime;
     }
 
-    // Update sale cap to keep it £25M
+    // Update sale cap to keep it in compliance with the whitepaper
     function increaseCap(uint256 newCap)
         public
         onlyOwner
