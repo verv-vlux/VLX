2e8d409071329968be552fc2667760831d1532da
jshabadoo <jshabadoo@protonmail.com>
2018-08-02 22:36:14 +0100

    Added correct gas price

--- truffle-config.js
+++ truffle-config.js
@@ -8,11 +8,13 @@ module.exports = {
       host: "127.0.0.1",
       port: 8546,
       network_id: "*",
+      gasPrice: 1000000000,
     },
     testnet: {
       host: "127.0.0.1",
       port: 8545,
       network_id: 3,
+      gasPrice: 30000000000,
       gas: util.gas.maxGas,
     },
     mainnet: {
